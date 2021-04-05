8dc611685d8f36ce446f2e52a40fb5a257c46b6e
Zack <zack4work@gmail.com>
2021-04-05 21:10:17 +0800

    test001冲突

--- app/src/main/java/io/dearzack/watermelon/MainActivity.java
+++ app/src/main/java/io/dearzack/watermelon/MainActivity.java
@@ -12,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startWatermelon();
-        String a = "冲突";
-        String b = "";
     }
 
     @Override
